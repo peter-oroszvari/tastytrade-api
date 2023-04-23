738dd1487bf35a28747d782c09219c4c65d68e5c
petycomppety <30414255+petycomppety@users.noreply.github.com>
2023-04-23 10:31:43 +0200

    Bumped to 0.8.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tastytrade-api",
-    version="0.8.0",
+    version="0.8.2",
     author="Peter Oroszvari",
     author_email="peter@oroszvari.hu",
     description="A Python client and SDK for the Tastytrade API",
