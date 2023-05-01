ce359adc5938959e00cb68a11a6f006c69b58d7e
petycomppety <30414255+petycomppety@users.noreply.github.com>
2023-05-01 12:49:27 +0200

    Modification of Quote mapping

--- tastytrade_api/streamer/dx_mapping.py
+++ tastytrade_api/streamer/dx_mapping.py
@@ -1,24 +1,24 @@
 import json
 
 class Quote:
-   
+
     def __init__(self, symbol, event_time, sequence, time_nano_part, bid_time, bid_exchange_code, bid_price, bid_size, ask_time, ask_exchange_code, ask_price, ask_size):
         """
-        Initializes a new instance of the class.
+        Initializes an instance of the class with the given parameters.
 
         Args:
-            symbol (Any): The symbol.
-            event_time (Any): The event time.
-            sequence (Any): The sequence.
-            time_nano_part (Any): The time nano part.
-            bid_time (Any): The bid time.
-            bid_exchange_code (Any): The bid exchange code.
-            bid_price (Any): The bid price.
-            bid_size (Any): The bid size.
-            ask_time (Any): The ask time.
-            ask_exchange_code (Any): The ask exchange code.
-            ask_price (Any): The ask price.
-            ask_size (Any): The ask size.
+            symbol (str): The symbol of the event.
+            event_time (int): The time of the event.
+            sequence (int): The sequence of the event.
+            time_nano_part (int): The nano part of the time of the event.
+            bid_time (int): The time of the bid.
+            bid_exchange_code (str): The exchange code of the bid.
+            bid_price (float): The price of the bid.
+            bid_size (int): The size of the bid.
+            ask_time (int): The time of the ask.
+            ask_exchange_code (str): The exchange code of the ask.
+            ask_price (float): The price of the ask.
+            ask_size (int): The size of the ask.
         """
         self.symbol = symbol
         self.event_time = event_time
@@ -32,28 +32,49 @@ class Quote:
         self.ask_exchange_code = ask_exchange_code
         self.ask_price = ask_price
         self.ask_size = ask_size
-        
+
     @classmethod
-    def from_json(cls, json_message):
-        try:
-            data_list = json.loads(json_message)[0]["data"][1]
-        except (KeyError, ValueError, IndexError):
-            raise ValueError("Invalid JSON message")
+    def from_list(cls, data_list):
+        """
+        Creates a list of Quote objects from a list of data.
+
+        Args:
+            cls (class): The class object.
+            data_list (list): The list of data to convert into Quote objects.
+
+        Returns:
+            list: The list of Quote objects created from the data.
+        """ 
         quotes = []
-        for i in range(0, len(data_list), 12):
-            quote_data = data_list[i:i+12]
-            quote = cls(*quote_data)
+        if not data_list:
+            print("Invalid data list received")
+            return quotes
+
+        # Check if the first element is a header, if yes, skip it
+        if isinstance(data_list[0], list) and len(data_list[0]) > 1 and data_list[0][0] == 'Quote' and data_list[0][1][0] == 'eventSymbol':
+            data_list = data_list[1:]
+
+        for i in range(0, len(data_list)):
+            quote_data = data_list[i]
+            if len(quote_data) != 12:
+                continue
+
+            symbol, event_time, sequence, time_nano_part, bid_time, bid_exchange_code, bid_price, bid_size, ask_time, ask_exchange_code, ask_price, ask_size = quote_data
+            bid_size = float(bid_size) if bid_size != 'NaN' else None
+            ask_size = float(ask_size) if ask_size != 'NaN' else None
+            quote = cls(symbol, event_time, sequence, time_nano_part, bid_time, bid_exchange_code, bid_price, bid_size, ask_time, ask_exchange_code, ask_price, ask_size)
             quotes.append(quote)
         return quotes
-    
+
+
+
     def __str__(self):
+        """
+        Return a string representation of the object.
+
+        Returns:
+            str: A string representing the object, including the symbol, event time,
+                sequence, time nano part, bid time, bid exchange code, bid price,
+                bid size, ask time, ask exchange code, ask price, and ask size.
+        """
         return f"Symbol: {self.symbol}, Event time: {self.event_time}, Sequence: {self.sequence}, Time nano part: {self.time_nano_part}, Bid time: {self.bid_time}, Bid exchange code: {self.bid_exchange_code}, Bid price: {self.bid_price}, Bid size: {self.bid_size}, Ask time: {self.ask_time}, Ask exchange code: {self.ask_exchange_code}, Ask price: {self.ask_price}, Ask size: {self.ask_size}"
-
-"""
-data1 = [{'data': ['Quote', ['AAPL', 0, 0, 0, 0, 'Q', 167.1, 439.0, 0, 'Q', 167.12, 602.0, 'TSLA', 0, 0, 0, 0, 'Q', 162.07, 100.0, 0, 'Q', 162.09, 100.0]], 'channel': '/service/data'}]
-data2 = [{'data': ['Quote', ['AAPL', 0, 0, 0, 0, 'Q', 167.11, 200.0, 0, 'Q', 167.12, 302.0]], 'channel': '/service/data'}]
-
-quotes1 = Quote.from_json(json.dumps(data1))
-for quote in quotes1:
-    print(quote)
-"""
