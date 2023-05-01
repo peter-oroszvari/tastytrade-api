5ba2bf4089a2c111c18a94d91e78ddbcd9ebd03e
petycomppety <30414255+petycomppety@users.noreply.github.com>
2023-05-01 13:00:48 +0200

    Bump to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tastytrade-api",
-    version="1.0 .0",
+    version="1.1.0",
     author="Peter Oroszvari",
     author_email="peter@oroszvari.hu",
     description="A Python client and SDK for the Tastytrade API",
